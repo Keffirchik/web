cee992ac94025a8b94ef56bd980e6d7929a465a0
Keffirchik <45791095+Keffirchik@users.noreply.github.com>
2019-04-22 12:03:42 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+mkdir /home/box/web/public/img /home/box/web/public/css /home/box/web/public/js /home/box/web/uploads /home/box/web/etc
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
