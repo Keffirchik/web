293a6c8aa89d1165ef929b6a9ad51b01b4456ae7
Keffirchik <45791095+Keffirchik@users.noreply.github.com>
2019-04-30 12:41:22 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
